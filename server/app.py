09a15bbb4ca6e0798620c7a083bd0fcb3f487825
RonnieInvestments <ronnieinvestments81@gmail.com>
2026-04-15 23:18:02 +0300

    Complete CRUD operation and validate login.

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
 from flask_bcrypt import bcrypt
 from models import *
 from flask_jwt_extended import create_access_token,  JWTManager, jwt_required
@@ -21,22 +21,75 @@ def home():
 
 @app.route("/entries", methods=["GET"])
 def get_journal_entries():
-    pass
+
+    entries = JournalEntry.query.all()
+
+    entries_dict = journal_schema.dump(entries, many=True)
+
+    return make_response(entries_dict, 200)
+
 
 @app.route("/entries", methods=["POST"])
 def add_journal_entry():
-    pass
+
+    try:
+        incoming_data = journal_schema.load(request.get_json(), many=True)
+
+        db.session.add_all(incoming_data)
+        db.session.commit()
+
+        #Serializing
+        incoming_data_dict = journal_schema.dump(request.get_json(), many=True)
+        return make_response(incoming_data_dict, 201)
+    
+    except ValidationError as e:
+        return make_response({"message":"Could not load the data"})
+
 
 @app.route("/entry/<int:id>", methods=["PATCH"])
-def update_journal_entry():
-    pass
+def update_journal_entry(id):
+
+    try:
+        #Deserialize
+        incoming_data = journal_schema.load(request.get_json(), many=False)
+
+        entry = journal_schema.query.filter_by(id).first()
+
+        if not entry:
+            return make_response({"message":f"Entry {id} not found in the database."}, 404)
+        
+        entry.title = incoming_data["title"]
+        entry.content = incoming_data["content"]
+
+        db.session.commit()
+
+        return make_response({"message":"Entry updated successfully"}, 201)
+    
+    except Exception as e:
+        return make_response({"error":f"Could not update data {e}"}, 404)
+    
 
 @app.route("/entry/<int:id>", methods=["GET"])
-def delete_journal_entry():
-    pass
+def delete_journal_entry(id):
+    
+    try:
+        entry = journal_schema.query.filter_by(id).first()
+
+        if not entry:
+            return make_response({"message":f"Entry {id} not found in the database."}, 404)
+        
+        #Delete if found
+        db.session.delete(id)
+        db.session.commit()
+
+        return make_response({}, 201)
+    
+    except Exception as e:
+        return make_response({"error":f"Could not delete entry {e}"}, 404)
 
 @app.route('/login', methods=['POST'])
 def login():
+    
     data = request.get_json()
     username = data['username']
     password = data['password']
@@ -49,3 +102,7 @@ def login():
         return jsonify({'message': 'Login Success', 'access_token': access_token})
     else:
         return jsonify({'message': 'Login Failed'}), 401
+    
+
+if __name__=="__main__":
+    app.run(debug=True)
